a07c8c62c2fe919e31a9ce6f2546cc9ed5e703dd
rustdesk <info@rustdesk.com>
2022-04-12 04:43:47 +0800

    disable search (not work in prod)

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+cd public; tar czf x *
+scp x sg:/tmp/
+ssh sg "sudo tar xzf /tmp/x -C /var/www/html/rustdesk.com/docs/ && /bin/rm /tmp/x && sudo chown www-data:www-data /var/www/html/rustdesk.com/docs/ -R"
+/bin/rm x
+cd -
