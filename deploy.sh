8e3948342de37f01400245c94a037b358da46605
rustdesk <info@rustdesk.com>
2022-04-12 05:01:29 +0800

    minify

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+hugo --minify
 cd public; tar czf x *
 scp x sg:/tmp/
 ssh sg "sudo tar xzf /tmp/x -C /var/www/html/rustdesk.com/docs/ && /bin/rm /tmp/x && sudo chown www-data:www-data /var/www/html/rustdesk.com/docs/ -R"
