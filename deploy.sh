4eb4f5086f56a4837ed2923304300e0872c69cba
Huabing Zhou <hzhou@MACdeMacBook-Air.local>
2023-01-05 11:25:40 +0800

    sg -> ot

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 hugo --minify
 cd public; tar czf x *
-scp x sg:/tmp/
-ssh sg "sudo rm -rf /var/www/html/rustdesk.com/docs/* && sudo tar xzf /tmp/x -C /var/www/html/rustdesk.com/docs/ && /bin/rm /tmp/x && sudo chown www-data:www-data /var/www/html/rustdesk.com/docs/ -R"
+scp x ot:/tmp/
+ssh ot "sudo rm -rf /var/www/html/rustdesk.com/docs/* && sudo tar xzf /tmp/x -C /var/www/html/rustdesk.com/docs/ && /bin/rm /tmp/x && sudo chown www-data:www-data /var/www/html/rustdesk.com/docs/ -R"
 /bin/rm x
 cd -
