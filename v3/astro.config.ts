082431f57e6e6f6aea12b04dd015d9a28c7646d3
rustdesk <info@rustdesk.com>
2024-10-11 14:33:16 +0800

    casourle

--- v3/astro.config.ts
+++ v3/astro.config.ts
@@ -15,6 +15,8 @@ import astrowind from './vendor/integration';
 
 import { readingTimeRemarkPlugin, responsiveTablesRehypePlugin, lazyImagesRehypePlugin } from './src/utils/frontmatter';
 
+import react from '@astrojs/react';
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const hasExternalScripts = false;
@@ -25,6 +27,7 @@ export default defineConfig({
   output: 'static',
 
   integrations: [
+    react(),
     tailwind({
       applyBaseStyles: false,
     }),
---
+++ v3/src/components/widgets/Carousel.jsx
@@ -0,0 +1,39 @@
+/* https://swiperjs.com/demos */
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Autoplay, Pagination, Scrollbar, Mousewheel } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+const Carousel = ({ list }) => {
+  return (
+    <Swiper
+      spaceBetween={0}
+      slidesPerView={1}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+      pagination={{ clickable: true }}
+      style={{
+        '--swiper-pagination-bullet-inactive-color': '#fff',
+        '--swiper-pagination-bullet-size': '12px',
+      }}
+      mousewheel={{ forceToAxis: true, sensitivity: 0.1, releaseOnEdges: true }}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: true,
+      }}
+      lazy={true}
+      scrollbar
+      modules={[Pagination, Autoplay, Scrollbar, Mousewheel]}
+    >
+      {list.map((item, i) => (
+        <SwiperSlide key={'p' + i}>
+          <img src={item.src} className={item.className} loading="lazy" alt={item.alt} />
+          <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default Carousel;
