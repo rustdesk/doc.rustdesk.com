fdc0973f15c8708ecc26fb2adc165792c3fc90dc
rustdesk <info@rustdesk.com>
2024-10-15 00:43:53 +0800

    fix zh and add more to compatible with v2

--- v3/deploy.sh
+++ v3/deploy.sh
@@ -6,7 +6,10 @@
  ln -s success/index.html success.html
  ln -s cancel/index.html cancel.html
  ln -s privacy/index.html privacy.html
- ln -s about/index.html about.html
+ ln -s team/index.html team.html
+ ln -s careers/index.html careers.html
+ ln -s open-source/index.html open-source.html
+ cp -rf ../zh ./
  tar czf x *
  scp x ot:/tmp/
  ssh ot "sudo tar xzf /tmp/x -C /var/www/html/v3 && /bin/rm /tmp/x && sudo chown www-data:www-data /var/www/html/v3 -R"
