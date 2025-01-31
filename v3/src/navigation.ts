8c2923c4aa0a1aa16bc0ceed2c092aaa8919fa7b
rustdesk <info@rustdesk.com>
2025-01-31 16:58:49 +0800

    2024 -> 2025

--- v3/src/navigation.ts
+++ v3/src/navigation.ts
@@ -257,6 +257,6 @@ export const footerData = (locale?: Lang) => {
       { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
       { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/rustdesk/rustdesk' },
     ],
-    footNote: '© 2024 Purslane',
+    footNote: '© 2025 Purslane',
   };
 }
