9ed739aca318c6beb456e3e35d629fd138903a9c
rustdesk <info@rustdesk.com>
2025-08-06 19:42:29 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-hugo --minify
 rm -rf public/*
+hugo --minify
 cd public; tar czf x *
 scp x ot:/tmp/
 ssh ot "sudo rm -rf /var/www/html/rustdesk.com/docs/* && sudo tar xzf /tmp/x -C /var/www/html/rustdesk.com/docs/ && /bin/rm /tmp/x && sudo chown www-data:www-data /var/www/html/rustdesk.com/docs/ -R"
